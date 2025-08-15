3891bed8ca98e9c0f33e877a2c56b2c9ffe1d031
Matt Hartstein <matthartstein@yahoo.com>
2025-08-14 23:22:54 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -2,8 +2,9 @@
 # Programming Language: Python
 # Libraries/Frameworks: Pygame, Pygame Mixer
 # Description: This is a 2D arcade-style game built with Pygame.
-#              The player controls a character (moved with arrow keys) on an 800x600 screen to avoid "Grinch" enemies (which reduce health by 10 on collision)
-#              and collect "Elf" enemies (which add 10 to the score) and "cookie" items (which restore health to 100).
+#              The player controls a character (moved with arrow keys) on an 800x600 screen to avoid "Grinch" enemies
+#              (which reduce health by 10 on collision)and collect "Elf" enemies (which add 10 to the score) and
+#              "cookie" items (which restore health to 100).
 #              The game features 8 enemies (60% Grinches, 40% Elves) and 1 cookie, all moving automatically with boundary bouncing.
 #              Background music and sound effects (e.g., attack and slurp sounds) enhance the experience.
 #              The score and health are displayed, and the game ends with a "GAME OVER!" message if health reaches 0.
